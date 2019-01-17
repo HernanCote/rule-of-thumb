77f2eaf6de8765e66cd8d70d6615289460f70459
Hernan Cote <hernancote@Hernans-MacBook-Air.local>
2019-01-16 20:42:23 -0500

    Created footer component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/navigation/navBar';
 import SideDrawer from './components/navigation/sideDrawer';
 import Backdrop from './components/common/backdrop';
 import Home from './components/pages/home';
+import Footer from './components/footer/footer';
 
 class App extends Component {
   state = {
@@ -32,6 +33,7 @@ class App extends Component {
         <SideDrawer show={this.state.sideDrawerOpen} />
         {backdrop}
         <Home />
+        <Footer />
       </React.Fragment>
     );
   }
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className='footer container'>
+      <div>Terms and Conditions</div>
+      <div>Privacy Policy</div>
+      <div>Contact Us</div>
+      <div>Follow Us</div>
+      <i className='fab fa-facebook' />
+      <i className='fab fa-twitter' />
+    </footer>
+  );
+};
+
+export default Footer;
